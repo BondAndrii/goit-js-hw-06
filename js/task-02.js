baf92b1b85341dd279cb58f75e67f58ed8a1e81b
BondAndrii <CitrusOk777@gmail.com>
2022-07-03 13:11:24 +0300

    Вирішує 2-гу задачу з 6ДЗ

--- js/task-02.js
+++ js/task-02.js
@@ -1,3 +1,15 @@
+// HTML містить порожній список ul#ingredients.
+// Напиши скрипт, який для кожного елемента масиву ingredients:
+
+// Створить окремий елемент <li>. Обов'язково використовуй метод document.createElement().
+// Додасть назву інгредієнта як його текстовий вміст.
+// Додасть елементу клас item.
+// Після чого, вставить усі <li> за одну операцію у список ul#ingredients.
+
+// План дій. Створити за допомогою JS  лішку разом з класом, далі за одну операцію потрібно додати всі лішки в UL. 
+// Скоріше за допомогою якогось циклу
+
+
 const ingredients = [
   'Potatoes',
   'Mushrooms',
@@ -6,3 +18,32 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+// console.log(ingredients);
+// const createFoo = function (array) {
+//   for (const arr of array) {
+//     console.log(arr);  
+//   }
+// }
+// createFoo(ingredients);
+
+// const ingToUpperCase = ingredients.map(ingredient => ingredient.toUpperCase());
+// console.log(ingToUpperCase);
+const ingredListRef = document.querySelector('#ingredients');
+// console.log(ingredListRef);
+
+const createUl = ingredients.map(ingredient => {
+  const createLi = document.createElement('li');  
+  createLi.classList.add('item');
+  createLi.textContent =ingredient;
+  console.log(createLi); 
+  return createLi;
+})
+console.log(createUl);
+ingredListRef.append(...createUl);
+
+// const cretLi = document.createElement('li');
+// cretLi.classList.add('item');
+// cretLi.textContent = "Пробний заголовок";
+// ingredListRef.appendChild(cretLi);
+// console.log(ingredListRef);
+
